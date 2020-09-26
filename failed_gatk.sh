8d2483aa836ff6dfac211837c62ae72e712e40ab
Abhinav Sharma <abhi18av@gmail.com>
2020-09-26 05:54:21 +0000

    failed bams

---
+++ failed_gatk.sh
@@ -0,0 +1,639 @@
+ls -la work/8a/be9aa4*
+
+ls -la work/90/5847c7*
+
+ls -la work/4b/c7cdde*
+
+ls -la work/be/6014a4*
+
+ls -la work/99/40d74c*
+
+ls -la work/68/a07981*
+
+ls -la work/e4/f2c372*
+
+ls -la work/28/c89f06*
+
+ls -la work/a9/438fca*
+
+ls -la work/9b/04cc83*
+
+ls -la work/e2/34ccfa*
+
+ls -la work/1c/32d534*
+
+ls -la work/c5/8985de*
+
+ls -la work/31/760210*
+
+ls -la work/3d/af2095*
+
+ls -la work/df/e85af7*
+
+ls -la work/c5/a571db*
+
+ls -la work/58/60cb93*
+
+ls -la work/97/0cfa28*
+
+ls -la work/55/023108*
+
+ls -la work/d4/d69ea9*
+
+ls -la work/da/990436*
+
+ls -la work/fa/d3bf14*
+
+ls -la work/85/8c5da0*
+
+ls -la work/03/fe1a36*
+
+ls -la work/b0/6e6828*
+
+ls -la work/be/acd699*
+
+ls -la work/28/1b5ccd*
+
+ls -la work/a4/31bcd1*
+
+ls -la work/88/821c72*
+
+ls -la work/3d/25b3b1*
+
+ls -la work/0e/ff56da*
+
+ls -la work/32/d20d50*
+
+ls -la work/65/687327*
+
+ls -la work/d1/6dcc4d*
+
+ls -la work/67/4d891f*
+
+ls -la work/5a/614317*
+
+ls -la work/90/ffa7f5*
+
+ls -la work/16/5d33bb*
+
+ls -la work/04/54f7b3*
+
+ls -la work/9c/460d60*
+
+ls -la work/93/ffa554*
+
+ls -la work/a5/15baa4*
+
+ls -la work/3e/e3d2d0*
+
+ls -la work/d8/ee3efe*
+
+ls -la work/bf/fa4239*
+
+ls -la work/6a/04e28b*
+
+ls -la work/d8/240b8c*
+
+ls -la work/99/251593*
+
+ls -la work/5e/63bd55*
+
+ls -la work/45/7c3360*
+
+ls -la work/04/f6632a*
+
+ls -la work/db/66f614*
+
+ls -la work/21/8fcc0a*
+
+ls -la work/d2/8677a4*
+
+ls -la work/2d/d9b03d*
+
+ls -la work/b7/6d019d*
+
+ls -la work/07/73896d*
+
+ls -la work/8f/60f07c*
+
+ls -la work/93/f079aa*
+
+ls -la work/b7/43835d*
+
+ls -la work/77/5ba16f*
+
+ls -la work/67/cc0e4c*
+
+ls -la work/b5/73805e*
+
+ls -la work/63/ffb066*
+
+ls -la work/6e/cbd578*
+
+ls -la work/54/f7d91f*
+
+ls -la work/52/ee5ace*
+
+ls -la work/ca/da458e*
+
+ls -la work/b9/6e1484*
+
+ls -la work/a9/4a9126*
+
+ls -la work/41/8e81a4*
+
+ls -la work/29/04c242*
+
+ls -la work/73/21c3c6*
+
+ls -la work/42/ae150a*
+
+ls -la work/2b/2d114e*
+
+ls -la work/0e/a9e679*
+
+ls -la work/a1/88d09b*
+
+ls -la work/f7/f32f9d*
+
+ls -la work/82/9099bb*
+
+ls -la work/87/2f84d8*
+
+ls -la work/4d/67d8dc*
+
+ls -la work/44/bb734c*
+
+ls -la work/19/e7c9be*
+
+ls -la work/75/c498e8*
+
+ls -la work/a9/378961*
+
+ls -la work/2f/2dd12f*
+
+ls -la work/fa/189e80*
+
+ls -la work/15/882fba*
+
+ls -la work/a8/66c7ac*
+
+ls -la work/24/e85bb6*
+
+ls -la work/17/0bbc6c*
+
+ls -la work/45/bdb155*
+
+ls -la work/ca/a3cdf3*
+
+ls -la work/33/be5123*
+
+ls -la work/65/5a4d6e*
+
+ls -la work/09/f9a951*
+
+ls -la work/76/aa59f0*
+
+ls -la work/5a/c198ce*
+
+ls -la work/68/82f390*
+
+ls -la work/de/4f9092*
+
+ls -la work/f7/70825f*
+
+ls -la work/24/c337cd*
+
+ls -la work/b6/853742*
+
+ls -la work/6d/ca59bf*
+
+ls -la work/c1/8d8615*
+
+ls -la work/ec/f1d6c1*
+
+ls -la work/d6/19707b*
+
+ls -la work/6b/94eb42*
+
+ls -la work/39/1161e5*
+
+ls -la work/76/1a60bf*
+
+ls -la work/c8/973622*
+
+ls -la work/70/d0c65a*
+
+ls -la work/65/f3bd42*
+
+ls -la work/9b/a5ec9b*
+
+ls -la work/63/c82bc8*
+
+ls -la work/2d/70c660*
+
+ls -la work/32/d249e9*
+
+ls -la work/26/842d05*
+
+ls -la work/5f/59db03*
+
+ls -la work/62/b9de03*
+
+ls -la work/f2/d65568*
+
+ls -la work/20/34885c*
+
+ls -la work/99/77be25*
+
+ls -la work/ab/1a103b*
+
+ls -la work/bb/c9d758*
+
+ls -la work/9a/fcc939*
+
+ls -la work/e4/feb6ae*
+
+ls -la work/9a/56e71c*
+
+ls -la work/11/4e7608*
+
+ls -la work/26/3d6f49*
+
+ls -la work/26/c96f1b*
+
+ls -la work/9c/98a9fb*
+
+ls -la work/bc/f6b616*
+
+ls -la work/98/b80217*
+
+ls -la work/8e/84d8b3*
+
+ls -la work/56/f0cef1*
+
+ls -la work/ee/181c0f*
+
+ls -la work/6d/73bc09*
+
+ls -la work/11/a6fffe*
+
+ls -la work/6a/392fb9*
+
+ls -la work/2c/dfa96c*
+
+ls -la work/e4/68257a*
+
+ls -la work/07/473980*
+
+ls -la work/dc/d99274*
+
+ls -la work/c2/d11e18*
+
+ls -la work/5b/997bed*
+
+ls -la work/f7/cb999a*
+
+ls -la work/31/8c81b6*
+
+ls -la work/28/793a4f*
+
+ls -la work/df/c54165*
+
+ls -la work/60/86db27*
+
+ls -la work/9e/1f9953*
+
+ls -la work/ce/0d8b7c*
+
+ls -la work/3d/1fc326*
+
+ls -la work/f0/cf4ef9*
+
+ls -la work/67/56c859*
+
+ls -la work/56/34a22f*
+
+ls -la work/48/33f9ad*
+
+ls -la work/e5/d6af8f*
+
+ls -la work/32/396e48*
+
+ls -la work/e2/2f390f*
+
+ls -la work/fa/9dcacf*
+
+ls -la work/a1/c61bf4*
+
+ls -la work/46/1ea480*
+
+ls -la work/95/96d505*
+
+ls -la work/f1/5b5c23*
+
+ls -la work/98/2e8ab8*
+
+ls -la work/2f/2cf191*
+
+ls -la work/9d/543760*
+
+ls -la work/f7/b5aa74*
+
+ls -la work/04/0e920d*
+
+ls -la work/9d/9a9033*
+
+ls -la work/85/5dc243*
+
+ls -la work/e2/c74538*
+
+ls -la work/a2/807265*
+
+ls -la work/f6/f4e1ba*
+
+ls -la work/fd/0ae310*
+
+ls -la work/45/bc430f*
+
+ls -la work/a3/8618c2*
+
+ls -la work/fa/9b74a4*
+
+ls -la work/b7/70ad85*
+
+ls -la work/a2/26c18a*
+
+ls -la work/7c/537a37*
+
+ls -la work/ef/494f36*
+
+ls -la work/5b/c7f0f8*
+
+ls -la work/b0/392dd2*
+
+ls -la work/fb/029ec8*
+
+ls -la work/11/c6f098*
+
+ls -la work/6c/fe85f5*
+
+ls -la work/68/0d4596*
+
+ls -la work/1f/8ea43d*
+
+ls -la work/db/9eb927*
+
+ls -la work/82/1e3ab1*
+
+ls -la work/16/5f5e76*
+
+ls -la work/cd/1aa52c*
+
+ls -la work/af/52fbb9*
+
+ls -la work/4e/df7442*
+
+ls -la work/65/2f9451*
+
+ls -la work/a8/02ee8e*
+
+ls -la work/ea/dc4bb3*
+
+ls -la work/7f/5f6b55*
+
+ls -la work/bc/610349*
+
+ls -la work/75/8dc8bd*
+
+ls -la work/e4/3ede9f*
+
+ls -la work/3b/9aebfd*
+
+ls -la work/72/2235ee*
+
+ls -la work/cf/ddc1c7*
+
+ls -la work/ca/977c4e*
+
+ls -la work/b3/0503e3*
+
+ls -la work/93/54be1f*
+
+ls -la work/6b/ccceb1*
+
+ls -la work/47/f32d3d*
+
+ls -la work/cc/48c7a3*
+
+ls -la work/a5/a5dfcc*
+
+ls -la work/f5/ed48aa*
+
+ls -la work/62/426f30*
+
+ls -la work/65/68b6ee*
+
+ls -la work/bc/9c9d9a*
+
+ls -la work/c8/59688a*
+
+ls -la work/10/0ffee8*
+
+ls -la work/8c/3e77f4*
+
+ls -la work/21/e0188e*
+
+ls -la work/e7/fa9d0d*
+
+ls -la work/0c/93acac*
+
+ls -la work/8d/448835*
+
+ls -la work/ad/ae2e70*
+
+ls -la work/8e/627b20*
+
+ls -la work/24/4acaf4*
+
+ls -la work/de/ce3f64*
+
+ls -la work/f9/890757*
+
+ls -la work/57/ebeb3c*
+
+ls -la work/fb/628b84*
+
+ls -la work/ce/b65f44*
+
+ls -la work/95/880296*
+
+ls -la work/c9/71fc5a*
+
+ls -la work/18/ef61bb*
+
+ls -la work/7e/f4b422*
+
+ls -la work/7c/a01cac*
+
+ls -la work/ec/b4407a*
+
+ls -la work/32/407afd*
+
+ls -la work/86/3c8c4f*
+
+ls -la work/0e/f03241*
+
+ls -la work/2a/f4c6fa*
+
+ls -la work/1d/a76c9a*
+
+ls -la work/c5/c4150a*
+
+ls -la work/13/c247ad*
+
+ls -la work/7e/23fc93*
+
+ls -la work/d1/9c6fdd*
+
+ls -la work/04/014e64*
+
+ls -la work/b1/b6d805*
+
+ls -la work/c2/cf492b*
+
+ls -la work/b3/203091*
+
+ls -la work/e0/64393c*
+
+ls -la work/2c/8fc2f8*
+
+ls -la work/ee/6a62ec*
+
+ls -la work/24/f9a8cc*
+
+ls -la work/1d/bc20b0*
+
+ls -la work/87/209613*
+
+ls -la work/fe/44cdcf*
+
+ls -la work/e0/1b3b99*
+
+ls -la work/3f/de6914*
+
+ls -la work/b5/f63222*
+
+ls -la work/84/a6accc*
+
+ls -la work/68/d7591b*
+
+ls -la work/87/215bfd*
+
+ls -la work/82/82cdc4*
+
+ls -la work/3b/cf1395*
+
+ls -la work/ae/1e7159*
+
+ls -la work/17/982478*
+
+ls -la work/5d/1c3654*
+
+ls -la work/62/062bf4*
+
+ls -la work/af/dd1af1*
+
+ls -la work/e2/40b030*
+
+ls -la work/df/787be3*
+
+ls -la work/d3/4688ca*
+
+ls -la work/e5/09ad0c*
+
+ls -la work/11/c88e09*
+
+ls -la work/ec/898046*
+
+ls -la work/06/9f469a*
+
+ls -la work/06/554ba5*
+
+ls -la work/b5/fdaca2*
+
+ls -la work/9b/2519ac*
+
+ls -la work/a8/63f3c9*
+
+ls -la work/f0/8bba44*
+
+ls -la work/0b/3d2751*
+
+ls -la work/0b/a3cece*
+
+ls -la work/87/9397b8*
+
+ls -la work/d2/fa36be*
+
+ls -la work/97/df2ef1*
+
+ls -la work/0d/81a73c*
+
+ls -la work/ae/bad231*
+
+ls -la work/6e/ed7628*
+
+ls -la work/52/6a8306*
+
+ls -la work/3a/018e5b*
+
+ls -la work/02/60ac99*
+
+ls -la work/2f/f38df8*
+
+ls -la work/04/69eb4c*
+
+ls -la work/5e/e07513*
+
+ls -la work/7c/b2254d*
+
+ls -la work/02/7384ae*
+
+ls -la work/45/577b75*
+
+ls -la work/89/3b4eea*
+
+ls -la work/48/195015*
+
+ls -la work/5f/b3d97b*
+
+ls -la work/0d/ff11b0*
+
+ls -la work/c8/54f5fe*
+
+ls -la work/41/76da86*
+
+ls -la work/5b/87cecb*
+
+ls -la work/7c/56ee5a*
+
+ls -la work/d8/ce0673*
+
+ls -la work/65/8f3541*
+
+ls -la work/6e/efdc48*
+
+ls -la work/1c/26e103*
+
+ls -la work/cd/f3f2af*
+
+ls -la work/5c/14ff1f*
+
+ls -la work/fe/dd1be2*
+
+ls -la work/0f/f28f56*
+
+ls -la work/d2/a0c2d0*
+
+ls -la work/30/9f869b*
